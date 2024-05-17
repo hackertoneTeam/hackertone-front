e8db2f12121e4ae5e3c361c32b2e209b07f1f276
Coupy <coupy1024@icloud.com>
2024-05-18 01:21:55 +0900

    [ADD] 아래 wave 추가

--- src/views/trip/TimelinePage.js
+++ src/views/trip/TimelinePage.js
@@ -8,6 +8,10 @@ const share = () => {
 
 };
 
+const edit = () => {
+
+};
+
 const TimelinePage = () => {
   return (
     <>
@@ -19,12 +23,12 @@ const TimelinePage = () => {
         </div>
         <h1>후쿠오카 여행</h1>
         <div>
-          <img src={process.env.PUBLIC_URL + "/images/icon_pen.svg"} alt="pen" />
-          <img className={style.correction} src={process.env.PUBLIC_URL + "/images/icon_share.svg"} alt="share" />
+          <img src={process.env.PUBLIC_URL + "/images/icon_pen.svg"} alt="pen" onClick={edit} />
+          <img className={style.correction} src={process.env.PUBLIC_URL + "/images/icon_share.svg"} alt="share" onClick={share} />
         </div>
       </div>
       <div className={style.dayHeader}>
-
+        
       </div>
       <div className={style.wave} style={{backgroundImage: `url(${process.env.PUBLIC_URL + "/images/wave.svg"})`}} />
         <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className={style.mapContainer}>
@@ -33,6 +37,10 @@ const TimelinePage = () => {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
         </MapContainer>
+    </div>
+    <div className={style.waveBottom} style={{backgroundImage: `url(${process.env.PUBLIC_URL + "/images/wave_bottom.svg"})`}} />
+    <div className={style.timelineContainer}>
+      
     </div>
     <FooterLayout />
     </>
