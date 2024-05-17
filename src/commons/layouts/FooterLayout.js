4207dbcbb5a2847abfdfca90e98824096d5adc60
박총명 <qkrri56@naver.com>
2024-05-17 21:37:59 +0900

    [ENH] Footer 작업

--- src/commons/layouts/FooterLayout.js
+++ src/commons/layouts/FooterLayout.js
@@ -1,9 +1,31 @@
 import style from './FooterLayout.module.css'
+import {Link} from "react-router-dom";
 
 const FooterLayout = () => {
   return (
     <>
-      <footer className={style.footer}></footer>
+      <footer className={style.footer}>
+        <ul>
+          <li className={style.active}>
+            <Link to="/">
+              <img src={process.env.PUBLIC_URL + "/images/icon_passport_active.svg"} alt="여권"/>
+              <p>여권</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <img src={process.env.PUBLIC_URL + "/images/icon_diary.svg"} alt="일기"/>
+              <p>일기</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <img src={process.env.PUBLIC_URL + "/images/icon_menu.svg"} alt="메뉴"/>
+              <p>메뉴</p>
+            </Link>
+          </li>
+        </ul>
+      </footer>
     </>
   )
 }
