24eefa4f505b4c1ec517a2288c24e500501aadcd
박총명 <qkrri56@naver.com>
2024-05-18 00:29:37 +0900

    [ENH] 로그인 디자인
    - Footer menu active 설정

--- src/commons/layouts/FooterLayout.js
+++ src/commons/layouts/FooterLayout.js
@@ -1,26 +1,37 @@
 import style from './FooterLayout.module.css'
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
 const FooterLayout = () => {
+  const location = useLocation();
+  console.log(location.pathname);
   return (
     <>
       <footer className={style.footer}>
         <ul>
-          <li className={style.active}>
+          <li className={location.pathname === '/' ? style.active : ""}>
             <Link to="/">
-              <img src={process.env.PUBLIC_URL + "/images/icon_passport_active.svg"} alt="여권"/>
+              {location.pathname === '/' ?
+                  <img src={process.env.PUBLIC_URL + "/images/icon_passport_active.svg"} alt="여권"/>:
+                  <img src={process.env.PUBLIC_URL + "/images/icon_passport.svg"} alt="여권"/>
+              }
               <p>여권</p>
             </Link>
           </li>
-          <li>
-            <Link to="/">
-              <img src={process.env.PUBLIC_URL + "/images/icon_diary.svg"} alt="일기"/>
+          <li className={location.pathname === '/timeline' ? style.active : ""}>
+            <Link to="/timeline">
+              {location.pathname === '/timeline' ?
+                <img src={process.env.PUBLIC_URL + "/images/icon_diary_active.svg"} alt="일기"/>:
+                <img src={process.env.PUBLIC_URL + "/images/icon_diary.svg"} alt="일기"/>
+              }
               <p>일기</p>
             </Link>
           </li>
-          <li>
-            <Link to="/">
-              <img src={process.env.PUBLIC_URL + "/images/icon_menu.svg"} alt="메뉴"/>
+          <li className={location.pathname === '/login' ? style.active : ""}>
+            <Link to="/login">
+              {location.pathname === '/login' ?
+                <img src={process.env.PUBLIC_URL + "/images/icon_menu_active.svg"} alt="메뉴"/>:
+                <img src={process.env.PUBLIC_URL + "/images/icon_menu.svg"} alt="메뉴"/>
+              }
               <p>메뉴</p>
             </Link>
           </li>
--- src/views/login/LoginPage.js
+++ src/views/login/LoginPage.js
@@ -1,21 +1,35 @@
 import {useRecoilValue} from "recoil";
 import {isLoggedInState} from "../../states/LoginState";
+import style from './LoginPage.module.css'
+import FooterLayout from "../../commons/layouts/FooterLayout";
 
 const LoginPage = () => {
   const REST_API_KEY = process.env.REACT_APP_KAKAO_API_TOKEN;
   const REDIRECT_URI = "http://localhost:3000/auth/kakao/callback";
   const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
   const isLoggedIn = useRecoilValue(isLoggedInState);
+  console.log(isLoggedIn)
   const loginHandler = () => {
     window.location.href = link;
   };
 
   return (
     <>
-      {isLoggedIn ?
-      <button onClick={loginHandler}>로그인</button>:
-      <button onClick={loginHandler}>로그아웃</button>
-      }
+      <main className={style.loginMain}>
+        <h1>로그인</h1>
+        <div className={style.buttonWrap}>
+        {isLoggedIn ?
+          <button onClick={loginHandler}>
+            <img src={process.env.PUBLIC_URL + "/images/icon_kakao.svg"} alt="kakao"/>
+            <p>로그아웃</p>
+          </button>:
+          <button onClick={loginHandler}>
+            <img src={process.env.PUBLIC_URL + "/images/icon_kakao.svg"} alt="kakao"/>
+            <p>카카오 로그인</p>
+          </button>}
+        </div>
+      </main>
+      <FooterLayout />
     </>
   )
 }
