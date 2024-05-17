6086b341a963bdcc8f255fe1154a3a37dfc71ea2
박총명 <qkrri56@naver.com>
2024-05-17 14:34:01 +0900

    [TST] api test

--- src/views/main/MainPage.js
+++ src/views/main/MainPage.js
@@ -1,13 +1,31 @@
 import HeaderLayout from "../../commons/layouts/HeaderLayout";
 import FooterLayout from "../../commons/layouts/FooterLayout";
 import style from './MainPage.module.css'
+import {test} from "../../apis/test";
+import {useNavigate} from "react-router-dom";
 
 const MainPage = () => {
+  const navigate = useNavigate();
+  const apiTest = () => {
+    let data = {
+      name: "박총명"
+    }
+    test(data).then(r => {
+      navigate('/');
+    }).catch((r) => {
+      navigate('/');
+      alert("오류가 발생하여습니다.문의하여 주십시오.");
+      console.log("Register Post Error : " + r);
+    });
+  }
+
+
   return (
     <>
       <HeaderLayout />
       <main>
         <h1 className={style.title}>하이</h1>
+        <button onClick={apiTest}></button>
       </main>
       <FooterLayout />
     </>
